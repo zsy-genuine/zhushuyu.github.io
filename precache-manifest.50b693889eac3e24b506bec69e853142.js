04a28e7ad5c8d9b51f3de5f7f5faf2858030a3b5
zsy <2210256943@qq.com>
2019-07-30 20:56:33 +0800

    'houtai'

---
+++ precache-manifest.50b693889eac3e24b506bec69e853142.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19a44a1a27d362978b9ef63801f17bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13c4cec04f6c24d56641",
+    "url": "./static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "f6945a08952527000e9b",
+    "url": "./static/css/main.9260919d.chunk.css"
+  },
+  {
+    "revision": "13c4cec04f6c24d56641",
+    "url": "./static/js/2.29bea284.chunk.js"
+  },
+  {
+    "revision": "f6945a08952527000e9b",
+    "url": "./static/js/main.8c8c1fa3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
